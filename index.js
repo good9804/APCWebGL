f74e9d137bf30375cb9f6ef8779f2cbc35efea84
good9804 <ggoggoni98@gmail.com>
2023-07-12 15:22:48 +0900

    변경

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 let express = require("express");
 let app = express();
-app.use(express.static("/public")); //public이라는 폴더 사용을 선언함
+app.use(express.static("public")); //public이라는 폴더 사용을 선언함
 
 app.listen(3005, function () {
   console.log("App is running on port 3005");
@@ -11,6 +11,6 @@ app.listen(3005, function () {
 /*app.get("/", function (req, res) {
   res.sendfile("public/index.html"); //public 폴더 안의 index.html을 사용
 });*/
-app.get('*', (req, res) => {
-  res.sendFile('index.html', {root: 'public'});
+app.get('/', (req, res) => {
+  res.sendFile('index.html', );
 });
